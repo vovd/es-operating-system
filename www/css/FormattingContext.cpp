vovd/es-operating-system

commit 39a7d74a8339ac94324de673e67a451a3b0ee790

(FormattingContext::clear) : Fix a bug.
git-svn-id: http://es-operating-system.googlecode.com/svn/trunk@2157 d15c3b8f-5b43-0410-be93-4d5a55ce2954

www/css/FormattingContext.cpp
@@ -276,8 +276,8 @@ float FormattingContext::clear(unsigned value)
             h = std::max(h, (*i)->remainingHeight);
         }
     }
-    if (0.0f < h)
-        updateRemainingHeight(h - getMargin());
+    // Note there could be a floating box whose remainingHeight is zero.
+    updateRemainingHeight(h - getMargin());
     assert(!(value & 1) || left.empty());
     assert(!(value & 2) || right.empty());
     return h;

commit 02938a03e5e5da4f7c99daddb84ee135d851787a

(FormattingContext::clear) : Fix a bug introduced in r2157; cf. margin-collapse-135.

www/css/FormattingContext.cpp
@@ -260,24 +260,34 @@ float FormattingContext::clear(unsigned value)
 {
     if (!value)
         return 0.0f;
-    float h = 0.0f;
+    float h = NAN;
     if (value & 1) {  // clear left
         for (auto i = left.begin(); i != left.end(); ++i) {
             float w = (*i)->getEffectiveTotalWidth();
             x -= w;
             leftover += w;
-            h = std::max(h, (*i)->remainingHeight);
+            if (isnan(h))
+                h = (*i)->remainingHeight;
+            else
+                h = std::max(h, (*i)->remainingHeight);
         }
     }
     if (value & 2) {  // clear right
         for (auto i = right.begin(); i != right.end(); ++i) {
             float w = (*i)->getEffectiveTotalWidth();
             leftover += w;
-            h = std::max(h, (*i)->remainingHeight);
+            if (isnan(h))
+                h = (*i)->remainingHeight;
+            else
+                h = std::max(h, (*i)->remainingHeight);
         }
     }
-    // Note there could be a floating box whose remainingHeight is zero.
-    updateRemainingHeight(h - getMargin());
+    if (isnan(h))
+        h = 0.0f;
+    else {
+        // Note there could be a floating box whose remainingHeight is zero.
+        updateRemainingHeight(h - getMargin());
+    }
     assert(!(value & 1) || left.empty());
     assert(!(value & 2) || right.empty());
     return h;