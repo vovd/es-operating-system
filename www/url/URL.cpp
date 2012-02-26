vovd/es-operating-system

commit bf7f1f81c07d3bf98e9c76147ecf8e34c9e9f22b

(URI::getHost) : Omit the trailing port information if the default port for the service requested.
git-svn-id: http://es-operating-system.googlecode.com/svn/trunk@2156 d15c3b8f-5b43-0410-be93-4d5a55ce2954

www/url/URL.h
@@ -63,6 +63,10 @@ class URL
     bool parse();
     bool parseRelative(const URL& base);
 
+    bool isPort80() const {
+        return portStart + 2 == portEnd && url[portStart] == '8' && url[portStart + 1] == '0';
+    }
+
 public:
     bool isEmpty() const {
         return url.empty();
@@ -77,6 +81,8 @@ public:
     }
     void setProtocol(std::u16string protocol);
     std::u16string getHost() const {
+        if (isPort80())
+            return getHostname();
         return url.substr(hostStart, hostEnd - hostStart);
     }
     void setHost(std::u16string host);

www/url/URI.h
@@ -41,6 +41,10 @@ class URI
 
     void clear();
 
+    bool isPort80() const {
+        return portStart + 2 == portEnd && uri[portStart] == '8' && uri[portStart + 1] == '0';
+    }
+
 public:
     bool isEmpty() const {
         return uri.empty();
@@ -54,6 +58,8 @@ public:
         return uri.substr(0, protocolEnd);
     }
     std::string getHost() const {
+        if (isPort80())
+            return getHostname();
         return uri.substr(hostStart, hostEnd - hostStart);
     }
     std::string getHostname() const {

commit 0d6f8fad6b6dcbdfc063391fed09da079eade0b7

(URL::parseHTTPRelative) : Fix a regression by r2156.

www/url/URL.cpp
@@ -164,7 +164,7 @@ bool URL::parseUnreservedChar(size_t& pos)
         ++pos;
         return true;
     }
-    
+
     return parseUCSChar(pos);
 }
 
@@ -527,7 +527,7 @@ bool URL::parseHTTPRelative(const URL& base)
     protocolEnd = base.protocolEnd;
     ptrdiff_t offset = 0;
     if (hostStart == hostEnd) {
-        targetURL += base.getHost();
+        targetURL += base.url.substr(base.hostStart, base.hostEnd - base.hostStart);
         hostStart = base.hostStart;
         hostEnd = base.hostEnd;
         hostnameStart = base.hostnameStart;
@@ -535,7 +535,7 @@ bool URL::parseHTTPRelative(const URL& base)
         portStart = base.portStart;
         portEnd = base.portEnd;
     } else {
-        targetURL += getHost();
+        targetURL += url.substr(hostStart, hostEnd - hostStart);
         ptrdiff_t d = base.hostStart - hostStart;
         hostStart += d;
         hostEnd += d;

www/url/URL.h
@@ -136,8 +136,6 @@ public:
         return (a == b) && !std::memcmp(url.data(), target.url.data(), a);
     }
 
-    std::u16string resolveURL(std::u16string url);
-
     URL() {}
     URL(const std::u16string& url);
     URL(const URL& base, const std::u16string& relative);